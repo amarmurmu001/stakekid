e3b24718cf9a22c504ed69984b9eb3de81150c6f
amarmurmu001 <iron57441@gmail.com>
2025-06-01 15:37:23 +0530

    refactor(RichTextEditor): simplify imports from MDXEditor package
    The imports from MDXEditor were restructured to use direct imports from the main package instead of individual plugin paths. This improves maintainability by reducing import clutter and making dependencies clearer.

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,26 +1,19 @@
 'use client';
 
-import { MDXEditor } from '@mdxeditor/editor';
+import { MDXEditor, headingsPlugin, listsPlugin, quotePlugin, thematicBreakPlugin, linkPlugin, imagePlugin, tablePlugin, codeBlockPlugin, toolbarPlugin, markdownShortcutPlugin } from '@mdxeditor/editor';
 import '@mdxeditor/editor/style.css';
-import { headingsPlugin } from '@mdxeditor/editor/plugins/headings';
-import { listsPlugin } from '@mdxeditor/editor/plugins/lists';
-import { quotePlugin } from '@mdxeditor/editor/plugins/quote';
-import { thematicBreakPlugin } from '@mdxeditor/editor/plugins/thematic-break';
-import { linkPlugin } from '@mdxeditor/editor/plugins/link';
-import { imagePlugin } from '@mdxeditor/editor/plugins/image';
-import { tablePlugin } from '@mdxeditor/editor/plugins/table';
-import { codeBlockPlugin } from '@mdxeditor/editor/plugins/code-block';
-import { toolbarPlugin } from '@mdxeditor/editor/plugins/toolbar';
-import { markdownShortcutPlugin } from '@mdxeditor/editor/plugins/markdown-shortcut';
-import { UndoRedo } from '@mdxeditor/editor/plugins/toolbar/components/UndoRedo';
-import { BoldItalicUnderlineToggles } from '@mdxeditor/editor/plugins/toolbar/components/BoldItalicUnderlineToggles';
-import { BlockTypeSelect } from '@mdxeditor/editor/plugins/toolbar/components/BlockTypeSelect';
-import { CreateLink } from '@mdxeditor/editor/plugins/toolbar/components/CreateLink';
-import { InsertImage } from '@mdxeditor/editor/plugins/toolbar/components/InsertImage';
-import { InsertTable } from '@mdxeditor/editor/plugins/toolbar/components/InsertTable';
-import { InsertThematicBreak } from '@mdxeditor/editor/plugins/toolbar/components/InsertThematicBreak';
-import { ListsToggle } from '@mdxeditor/editor/plugins/toolbar/components/ListsToggle';
-import { CodeToggle } from '@mdxeditor/editor/plugins/toolbar/components/CodeToggle';
+import {
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  BlockTypeSelect,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  ListsToggle,
+  CodeToggle
+} from '@mdxeditor/editor';
+
 
 interface RichTextEditorProps {
   markdown: string;
